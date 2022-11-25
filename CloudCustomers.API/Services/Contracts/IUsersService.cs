d9995f673950a2775f5aabd2e835d13f79f2ad2c
MiroslavPukovski <miroslav.pukovski@gmail.com>
2022-11-25 19:55:27 +0200

    Add project files.

---
+++ CloudCustomers.API/Services/Contracts/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using CloudCustomers.API.Models;
+
+namespace CloudCustomers.API.Services.Contracts
+{
+    public interface IUsersService
+    {
+        public Task<List<User>> GetAllUsers();
+    }
+}
---
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,67 @@
+﻿using CloudCustomers.API.Models;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public static class UsersFixture
+    {
+        public static List<User> GetTestUsers() =>
+            new()
+            {
+                new User
+                {
+                    Name = "Test User 1",
+                    Email = "test.user.1@test.com",
+                    Address = new Address
+                    {
+                        Street = "123 Market St",
+                        City = "New York",
+                        ZipCode = "123456"
+                    }
+                },
+                new User
+                {
+                    Name = "Test User 2",
+                    Email = "test.user.2@test.com",
+                    Address = new Address
+                    {
+                        Street = "Leliju 2",
+                        City = "Vilnius",
+                        ZipCode = "LT12356"
+                    }
+                },
+                new User
+                {
+                    Name = "Test User 3",
+                    Email = "test.user.3@test.com",
+                    Address = new Address
+                    {
+                        Street = "Moscow 3",
+                        City = "Moscow",
+                        ZipCode = "58965"
+                    }
+                },
+                new User
+                {
+                    Name = "Test User 4",
+                    Email = "test.user.4@test.com",
+                    Address = new Address
+                    {
+                        Street = "Oceanside 25",
+                        City = "Maiami",
+                        ZipCode = "9852"
+                    }
+                },
+                new User
+                {
+                    Name = "Test User 5",
+                    Email = "test.user.5@test.com",
+                    Address = new Address
+                    {
+                        Street = "Kongo 896",
+                        City = "Tokyo",
+                        ZipCode = "52893"
+                    }
+                }
+            };
+    }
+}
